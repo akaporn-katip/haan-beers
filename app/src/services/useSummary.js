4cfe9ba01d9ef36b04acf5a5d1231205897a2158
akaporn-katip <akaporn.katip@gmail.com>
2024-01-03 10:07:43 +0700

    refactor getBill in summary function

--- app/src/services/useSummary.js
+++ app/src/services/useSummary.js
@@ -1,7 +1,6 @@
 import { doc, getDoc } from "firebase/firestore";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { db } from "../firebase/firestore";
-import { calculate_bill } from "../lib/calculate";
 
 export default function useSummary() {
   const [isLoading, setIsLoading] = useState(false);
@@ -17,30 +16,30 @@ export default function useSummary() {
       person: [],
     },
   ]);
+  const [summary, setSummary] = useState({ summary: "", person: [] });
 
-  async function getBill(uid, docId) {
-    try {
-      setIsLoading(true);
-      const docRef = doc(db, "user", uid, "bill", docId);
-      const docSnap = await getDoc(docRef);
-      setIsNotFound(!docSnap.exists());
-      if (docSnap.exists()) {
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 1000);
-        const data = docSnap.data();
-        setBillName(data.billName);
-        setOrderLine(data.orderLine);
-      }
-      setIsLoading(false);
-      return null;
-    } catch (error) {
-      setIsLoading(false);
-      console.error(error);
-    }
+  function getBill(uid, docId) {
+    setIsLoading(true);
+    const docRef = doc(db, "user", uid, "bill", docId);
+    getDoc(docRef)
+      .then((docSnap) => {
+        setIsNotFound(!docSnap.exists());
+        if (docSnap.exists()) {
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 1000);
+          const data = docSnap.data();
+          setBillName(data.billName);
+          setOrderLine(data.orderLine);
+          setSummary(data.summary);
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.error(error);
+      });
   }
-
-  const summary = useMemo(() => calculate_bill(orderLine), [orderLine]);
 
   return { getBill, isLoading, isNotFound, billName, orderLine, summary };
 }
